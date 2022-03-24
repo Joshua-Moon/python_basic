43a1990947b31438dcc491bd71933381ad0a19eb
Joshua-Moon <joshua8565@gmail.com>
2022-03-24 11:16:12 +0900

    구현: 6강 변수

---
+++ 6_variable.py
@@ -0,0 +1,15 @@
+# 변수(데이터의 이름)는 왜 필요한가?
+# 컴퓨터는 괜찮지만 사람이 작업할 때 보기 좋도록 하기위해(이해 + 가독성 향샹!)
+print(10000 * 0.1)
+
+price = 10000
+vat_rate = 0.1
+vat = price * vat_rate
+print(vat)
+
+# 반복작업을 하지 않게 해준다.
+name = 'egoing'
+message = 'hi, '+name+' .... bye, '+name+'.'
+print(message)
+
+
