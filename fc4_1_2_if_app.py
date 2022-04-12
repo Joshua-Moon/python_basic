486d5e49e7a7245526d4414acefd1c2a2b8665ef
Joshua-Moon <joshua8565@gmail.com>
2022-04-12 09:58:48 +0900

    fc4. 조건문

---
+++ fc4_1_2_if_app.py
@@ -0,0 +1,4 @@
+input_id = input('id : ')
+id = 'joshua'
+if input_id == id:
+    print('Welcome')
---
+++ fc4_2_2_if_app.py
@@ -0,0 +1,8 @@
+input_id = input('id : ')
+id = 'joshua'
+if input_id == id:
+    print('Welcome')
+# if input_id != id:
+#     print('Who?')
+else:
+    print('Who?')
